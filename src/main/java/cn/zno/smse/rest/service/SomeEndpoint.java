69aa2d1e6c8497aff7d8c881d19846019784a4cf
热线-郑兴广 <witaste@qq.com>
2015-10-20 14:08:36 +0800

    增加系统监控 和 web service

--- src/main/java/cn/zno/smse/rest/service/SomeEndpoint.java
+++ src/main/java/cn/zno/smse/rest/service/SomeEndpoint.java
@@ -1,9 +1,19 @@
 package cn.zno.smse.rest.service;
 
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 
 
+@Path("/some")
 public interface SomeEndpoint {
 	
-		
-
+	@GET
+	@Path("/ping/{input}")
+	@Produces({ MediaType.TEXT_PLAIN})
+	@Consumes({ MediaType.TEXT_PLAIN})
+	public String ping(@PathParam("input") String input);
 }
--- src/main/java/cn/zno/smse/rest/service/impl/SomeEndpointImpl.java
+++ src/main/java/cn/zno/smse/rest/service/impl/SomeEndpointImpl.java
@@ -1,9 +1,22 @@
 package cn.zno.smse.rest.service.impl;
 
+import javax.jws.WebService;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import cn.zno.smse.rest.service.SomeEndpoint;
 
+@WebService(endpointInterface = "cn.zno.smse.rest.service.SomeEndpoint")
 public class SomeEndpointImpl implements SomeEndpoint {
 
-	
+	final Logger logger = LoggerFactory.getLogger(getClass());
+
+	@Override
+	public String ping(String input) {
+		String msg = "hi ," + input;
+		logger.info(msg);
+		return msg;
+	}
 
 }
